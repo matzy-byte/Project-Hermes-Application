a261f39f2e0341d9a16013f6d7a350b7c7e73634
Bastian Weiler <bastian.weiler04@gmail.com>
2025-04-04 14:11:44 +0200

    godot project init

---
+++ frontend/SessionManager.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+// class for connecting to server websocket
+public partial class SessionManager : Node
+{
+    private string connectionString;
+
+    public override void _Ready()
+    {
+        
+    }
+}
